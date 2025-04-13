709ca491c6c2582318361ff7fcad06f4b576207f
FreddyBI <71754323+FreddyBI@users.noreply.github.com>
2025-04-13 18:06:07 +0200

    Update micropython.mk

--- usercmodule/micropython.mk
+++ usercmodule/micropython.mk
@@ -1,1 +1,8 @@
+CFBI_MOD_DIR := $(USERMOD_DIR)
 
+# Add all C files to SRC_USERMOD.
+SRC_USERMOD += $(CFBI_MOD_DIR)/cfbi.c
+
+# We can add our module folder to include paths if needed
+# This is not actually needed in this CFBI.
+CFLAGS_USERMOD += -I$(CFBI_MOD_DIR)
