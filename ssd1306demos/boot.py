21661f5d28ef6e3da4d591a4ac4809ed65b4532d
FreddyBI <71754323+FreddyBI@users.noreply.github.com>
2025-04-13 18:30:45 +0200

    Create boot.py

---
+++ ssd1306demos/boot.py
@@ -0,0 +1,26 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+#import webrepl
+#webrepl.start()
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+#import webrepl
+#webrepl.start()
+from machine import Pin,I2C,SoftI2C,freq
+from ssd1306fbi import SSD1306_I2C
+from os import mount,listdir,umount
+from time import sleep
+freq(240000000) # set the CPU frequency to 240 MHz
+
+i2c=SoftI2C(scl=Pin(22), sda=Pin(21))
+
+# Screen constant
+WIDTH = const(128)
+HEIGHT = const(64)
+LINE_HEIGHT = const(8)
+
+# create the display
+o=SSD1306_I2C(WIDTH,HEIGHT,i2c)
+
