8f865f66850aaf90299b6525e355276acdaabde2
FreddyBI <71754323+FreddyBI@users.noreply.github.com>
2025-04-13 18:04:01 +0200

    Update micropython.cmake

--- usercmodule/micropython.cmake
+++ usercmodule/micropython.cmake
@@ -1,1 +1,17 @@
+# Create an INTERFACE library for our C module.
 
+add_library(usermod_cfbi INTERFACE)
+
+# Add our source files to the lib
+
+target_sources(usermod_cfbi INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}/cfbi.c)
+
+# Add the current directory as an include directory.
+
+target_include_directories(usermod_cfbi INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR})
+
+# Link our INTERFACE library to the usermod target.
+
+target_link_libraries(usermod INTERFACE usermod_cfbi)
